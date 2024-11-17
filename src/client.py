c9cc59ebcdd4ec1c3887cf564fe0995439fde93e
pratapgovind <71212540+pratapgovind@users.noreply.github.com>
2024-11-17 18:59:42 +0100

    test commit

--- src/client.py
+++ src/client.py
@@ -39,7 +39,7 @@ def generate_summary(resumes, skills, api, model="llama3-8b-8192"):
     prompt = (
         f"You are an AI-powered hiring assistant tasked with finding the most suitable candidates from the following resumes. "
         f"Each resume is represented by its full text, and your job is to provide the names of the top candidates who best match the following required skills: {', '.join(skills)}. "
-        f"Extract and list only the names based on the highest similarity in skills without any additional context."
+        f"Extract and list only the names and email Ids based on the highest similarity in skills without any additional context."
     )
 
     client = Groq(api_key=api)
